eriksl/esp32

commit 96791c0eac8d190cca8db05acdedfdd082b21439

ramdisk: do not reclaim blocks when a truncating file.
This will make the memory fragmented badly, over time.

It should be resolved by issueing a periodic garbage collector.

The file could still be deleted/erased when the space is required.

main/ramdisk.c
@@ -174,29 +174,8 @@ static file_metadata_t *metadata_from_fd(vfs_ramdisk_context_t *context, int fd)
 
 static void file_truncate(file_metadata_t *meta, unsigned int length)
 {
-	unsigned int datablock;
-
-	assert(inited);
-	assert(block_ptr_size == (sizeof(meta->datablocks) / sizeof(*meta->datablocks)));
-
 	if(length < meta->length)
-	{
-		if(length == 0)
-			datablock = 0;
-		else
-			datablock = ((length - 1) / block_size) + 1;
-
-		for(; datablock < block_ptr_size; datablock++)
-		{
-			if(meta->datablocks[datablock])
-			{
-				free(meta->datablocks[datablock]);
-				meta->datablocks[datablock] = (data_block_t *)0;
-			}
-		}
-
-		meta->length = length;
-	}
+		length = meta->length;
 }
 
 static void file_stat(const vfs_ramdisk_context_t *context, const file_metadata_t *meta, int ix, struct stat *st)

commit c683ad71f3ef9ad4b7cb34f3fa067625ec94de63

ramdisk: fix memory leak introduced with 96791c0eac8d190cca8db05acdedfdd082b21439.

main/ramdisk.c
@@ -514,6 +514,8 @@ static int ramdisk_unlink(void *ctx, const char *filename)
 	file_metadata_t *meta, *parent;
 	const char *relpath;
 	struct stat statb;
+	unsigned int datablock_index;
+	data_block_t **datablock;
 
 	assert(inited);
 
@@ -539,7 +541,16 @@ static int ramdisk_unlink(void *ctx, const char *filename)
 		return(-1);
 	}
 
-	file_truncate(meta, 0);
+	for(datablock_index = 0; datablock_index < block_ptr_size; datablock_index++)
+	{
+		datablock = &meta->datablocks[datablock_index];
+
+		if(*datablock)
+		{
+			free(*datablock);
+			*datablock = (data_block_t *)0;
+		}
+	}
 
 	if(meta == context->root)
 		context->root = meta->next;
@@ -559,6 +570,7 @@ static int ramdisk_unlink(void *ctx, const char *filename)
 		parent->next = meta->next;
 	}
 
+	meta->length = 0;
 	meta->magic_word_1 = meta_magic_word_1 ^ 0xffffffff;
 	meta->magic_word_2 = meta_magic_word_2 ^ 0xffffffff;
 